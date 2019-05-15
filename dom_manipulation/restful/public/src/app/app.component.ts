0780ea78546cfb9bf2647eea981d54b045b0a1f7
Brett Jory <bjory7@gmail.com>
2019-05-15 11:44:06 -0700

    finished assignment restful tasks continued

---
+++ dom_manipulation/restful/public/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service' ;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'MEAN';
+  tasks = [];
+
+  constructor( private _httpService : HttpService) { }
+
+  ngOnInit() {
+    this.getTasksFromService();
+  }
+
+  getTasksFromService(){
+    let observable = this._httpService.getTasks();
+    observable.subscribe( data => {
+      console.log( 'component got tasks!' , data );
+      this.tasks = data.data;
+    } );
+  }
+
+}

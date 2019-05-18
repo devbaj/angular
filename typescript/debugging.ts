f2ecd1dcd1537edcefa3bd0601c437ef6fc2b7b0
Brett Jory <bjory7@gmail.com>
2019-05-18 13:40:25 -0700

    fixed dedugging assignment

--- typescript/debugging.ts
+++ typescript/debugging.ts
@@ -99,13 +99,13 @@ var math = (x, y) => {
 // 7
 class Elephant {
 	constructor(public age: number){}
-	birthday = function(){
-		this.age++;
+	birthday = () => {
+        this.age++;
 	}
 }
 var babar = new Elephant(8);
 setTimeout(babar.birthday, 1000)
 setTimeout(function(){
 	console.log(`Babar's age is ${babar.age}.`)
-	}, 2000)
+}, 2000)
 // Why didn't babar's age change? Fix this by using an arrow function in the Elephant class.
